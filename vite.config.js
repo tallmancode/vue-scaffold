7d478db6d070b4e553a4b013f02a84389ff20a6c
TallmanCode <steve.stewart2612@gmail.com>
2023-04-14 12:04:08 +0200

    Icons and Styling

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
                 exports: "named",
                 globals: {
                     vue: "Vue",
+                    "vue-router": "VueRouter",
                 },
             },
         },
